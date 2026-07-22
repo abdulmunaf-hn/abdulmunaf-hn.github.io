51ad35efb4976b329e1d8e87617409e11504e9f0
abdulmunaf-hn <hnamunaf@gmail.com>
2026-07-22 22:34:26 +0530

    add the projects

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-var popupoverlay = document.querySelector(".popup-overlay");
-var popupbox = document.querySelector(".popup-box");
-var addpopupbutton = document.getElementById("add-popup-btn");
+let popupoverlay = document.querySelector(".popup-overlay");
+let popupbox = document.querySelector(".popup-box");
+let addpopupbutton = document.getElementById("add-popup-btn");
 
-addpopupbutton.addEventlistener("click", function () {
-  popupoverlay.Style.disply = "block";
-  popupbox.Style.display = "block";
-});
+addpopupbutton.addEventListener("click", function(){
+  popupoverlay.style.display="block"
+  popupbox.style.display="block"
+})
+
+let cancelbutton = document.querySelector(".cancel-btn")
+cancelbutton.addEventListener("click",function(event){
+    event.preventDefault()
+ popupoverlay.style.display="none"
+  popupbox.style.display="none"
+
+})
+
+//select container,submit-btn,project-name-input,project-description
+let container=document.querySelector(".container")
+let addprojecter=document.querySelector(".submit-btn")
+let projectnemeinput=document.getElementById("project-neme-input")
+let projectdescription=document.getElementById("project-description")
+
+
+addprojecter=addEventListener("click",function(event){
+
+  event.preventDefault()
+  let div=document.createElement("div")
+  div.setAttribute("class","project-container")
+  div.innerHTML=`<h1>${projectnemeinput.value}</h1>`
+  container.append(div)
+  popupoverlay.style.display="none"
+  popupbox.style.display="none"
+  
+
+})
